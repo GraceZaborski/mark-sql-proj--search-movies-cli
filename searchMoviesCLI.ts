5f60f9dfff6f6341bdf5a9d5872a15e7a6bbdebe
Grace Zaborski <grace.zaborski@beamery.com>
2021-11-26 15:55:18 +0000

    Connecting to database

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -1,21 +1,15 @@
 import { question } from "readline-sync";
 import { Client } from "pg";
 
-console.log("Search for a movie or press q to quit");
+console.log("Search for a movie or press 'q' to quit");
+connectOMDB();
 
 //As your database is on your local machine, with default port,
 //and default username and password,
 //we only need to specify the (non-default) database name.
-async function searchOMDB() {
+async function connectOMDB() {
     const client = new Client({ database: 'omdb' });
     await client.connect();
-
-    const result = await client.query("SELECT * from casts where movie_id = $1", [
-        17,
-    ]);
-    console.table(result.rows);
     await client.end();
 }
-console.log("Welcome to search-movies-cli!");
 
-searchOMDB();
