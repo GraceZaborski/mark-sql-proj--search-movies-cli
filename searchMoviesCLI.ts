c880cceaf30199bff94821a9871ad9a4c2fd5872
Grace Zaborski <grace.zaborski@beamery.com>
2021-11-26 16:33:03 +0000

    Case-insensitive search

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -3,18 +3,23 @@ import { Client } from "pg";
 const readlineSync = require('readline-sync');
 
 
-console.log("Search for a movie or press 'q' to quit");
-connectOMDB();
-
-var userName = readlineSync.question('May I have your name? ');
-console.log('Hi ' + userName + '!');
+let userReturn = readlineSync.question("Search for a movie or press 'q' to quit: ");
+console.log(`You entered ${userReturn}`);
+if (userReturn != 'q') {
+    connectAndSearchOMDB()
+} else {
+    console.log("Server not connected.")
+}
 
 //As your database is on your local machine, with default port,
 //and default username and password,
 //we only need to specify the (non-default) database name.
-async function connectOMDB() {
+async function connectAndSearchOMDB() {
     const client = new Client({ database: 'omdb' });
     await client.connect();
+    const searchResult = await client.query
+        ("SELECT DISTINCT movie_id, movie_name from casts_view where lower(movie_name) like $1 LIMIT 10", [`%${userReturn.toLowerCase()}%`]);
+    console.table(searchResult.rows);
     await client.end();
 }
 
