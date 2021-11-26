507bc798f0281ebc56ae94bd3537a5abefbf871f
Grace Zaborski <grace.zaborski@beamery.com>
2021-11-26 16:04:41 +0000

    Added readline sync

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -1,8 +1,13 @@
 import { question } from "readline-sync";
 import { Client } from "pg";
+const readlineSync = require('readline-sync');
+
 
 console.log("Search for a movie or press 'q' to quit");
 connectOMDB();
+
+var userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
 
 //As your database is on your local machine, with default port,
 //and default username and password,
@@ -12,4 +17,6 @@ async function connectOMDB() {
     await client.connect();
     await client.end();
 }
+
+
 
