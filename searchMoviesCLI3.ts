b330b5186476666686c9a20073629a9487f77970
Grace Zaborski <grace.zaborski@beamery.com>
2021-11-27 21:34:17 +0000

    Abstracting into functions, fixing navigation

--- searchMoviesCLI3.ts
+++ searchMoviesCLI3.ts
@@ -1,21 +1,33 @@
-import { question } from "readline-sync";
+import { question, questionInt } from "readline-sync";
 import { Client } from "pg";
 const readlineSync = require('readline-sync');
 
-console.log("Welcome to search-movies-cli!\n\n[1]Search\n[2]See Favourites\n[3]Quit\n")
+console.log('Welcome to search-movies-cli!')
+connectOMDB()
 
-let action = readlineSync.question("Choose an action! [1, 2, 3]: ")
-if (action === '1') {
-    connectAndSearchOMDB()
-} else {
-    console.log("Server not connected.")
+
+async function connectOMDB() {
+    const client = new Client({ database: 'omdb' })
+    await client.connect();
+    chooseOption(client)
 }
 
-console.log('hello')
 
-async function connectAndSearchOMDB() {
-    const client = new Client({ database: 'omdb' });
-    await client.connect();
+function chooseOption(client: Client) {
+    let action = readlineSync.question("Choose an action! \n\n[1]Search\n[2]See Favourites\n[3]Quit");
+    if (action === '1') {
+        searchOMDB(client)
+        console.log('1')
+    } else if (action === '2') {
+        showFavourites(client)
+        console.log('2')
+    } else if (action === '3') {
+        quit(client)
+        console.log('3')
+    }
+}
+
+async function searchOMDB(client: Client) {
     let userReturn = readlineSync.question("Search term: ");
     const searchResult = await client.query
         ("SELECT DISTINCT movie_id, movie_name, date from casts_view where kind = 'movie' AND lower(movie_name) like $1 ORDER BY date desc LIMIT 8", [`%${userReturn.toLowerCase()}%`]);
@@ -32,10 +44,18 @@ async function connectAndSearchOMDB() {
         "INSERT INTO fave_movies_names (movie_id, movie_name) VALUES ($1, $2)";
     const values = [`${searchResult.rows[userFavourite - 1].movie_id}`, `${searchResult.rows[userFavourite - 1].movie_name}`];
     const favouriteTable = await client.query(favouriteTableQuery, values);
-    const favouriteTableReturn = await client.query("SELECT * FROM fave_movies_names LIMIT 8");
-    console.table(favouriteTableReturn.rows)
-    await client.end();
+    chooseOption(client)
 }
 
+async function showFavourites(client: Client) {
+    console.log('Here are your saved favourites!')
+    const favouriteTableReturn = await client.query("SELECT * FROM fave_movies_names LIMIT 8");
+    console.table(favouriteTableReturn.rows)
+    chooseOption(client)
+}
+
+async function quit(client: Client) {
+    await client.end();
+}
 
 
